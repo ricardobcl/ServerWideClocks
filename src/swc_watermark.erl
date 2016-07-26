6b3174e629fff92c6bb0a3e4ba57ca3ebec86147
Ricardo Gonçalves <tome.wave@gmail.com>
2016-07-26 14:41:53 +0100

    Add new watermark function to join a node clock info to the watermark directly.

--- src/swc_watermark.erl
+++ src/swc_watermark.erl
@@ -20,6 +20,7 @@
 %% API exports
 -export([ new/0
         , add/3
+        , add/4
         , min/2
         , peers/1
         , get/3
@@ -31,9 +32,23 @@
 new() ->
     orddict:new().
 
+-spec add(vv_matrix(), id(), bvv()) -> vv_matrix().
+add(M, EntryId, C) ->
+    Peers = case orddict:find(EntryId, M) of
+                error -> peers(M);
+                {ok, E} -> peers(M) ++ swc_vv:ids(E)
+            end,
+    VV1 = orddict:map(fun (_,{B,_}) -> B end, C),
+    VV2 = orddict:filter(fun (K,_) -> lists:member(K,Peers) end, VV1),
+    orddict:update(
+        EntryId,
+        fun (OldVV) -> swc_vv:join(OldVV, VV2) end,
+        VV2, M).
 
--spec add(vv_matrix(), id(), {id(), counter()}) -> vv_matrix().
-add(M, EntryId, Top) ->
+
+-spec add(vv_matrix(), id(), id(), counter()) -> vv_matrix().
+add(M, EntryId, PeerId, Counter) ->
+    Top = {PeerId, Counter},
     orddict:update(
         EntryId,
         fun (OldVV) -> swc_vv:add(OldVV, Top) end,
@@ -76,24 +91,35 @@ delete_peer(M, Id) ->
 -ifdef(TEST).
 
 add_test() ->
+    C1 = [{"a",{12,0}}, {"b",{7,0}}, {"c",{4,0}}, {"d",{5,0}}, {"e",{5,0}}, {"f",{7,10}}, {"g",{5,10}}, {"h",{5,14}}],
+    C2 = [{"a",{5,14}}, {"b",{5,14}}, {"c",{50,14}}, {"d",{5,14}}, {"e",{15,0}}, {"f",{5,14}}, {"g",{7,10}}, {"h",{7,10}}],
     M = new(),
-    M1 = add(M, "a", {"b",4}),
-    M2 = add(M1, "a", {"c",10}),
-    M3 = add(M2, "c", {"c",2}),
-    M4 = add(M3, "c", {"c",20}),
-    M5 = add(M4, "c", {"c",15}),
+    M1 = add(M, "a", "b",4),
+    M2 = add(M1, "a", "c",10),
+    M3 = add(M2, "c", "c",2),
+    M4 = add(M3, "c", "c",20),
+    M5 = add(M4, "c", "c",15),
+    M6 = add(M5, "c", C1),
+    M7 = add(M5, "c", C2),
+    M8 = add(M5, "a", C1),
+    M9 = add(M5, "a", C2),
     ?assertEqual( M1, [{"a",[{"b",4}]}]),
     ?assertEqual( M2, [{"a",[{"b",4}, {"c",10}]}]),
     ?assertEqual( M3, [{"a",[{"b",4}, {"c",10}]}, {"c",[{"c",2}]}]),
     ?assertEqual( M4, [{"a",[{"b",4}, {"c",10}]}, {"c",[{"c",20}]}]),
-    ?assertEqual( M4, M5).
+    ?assertEqual( M4, M5),
+    ?assertEqual( M6, [{"a",[{"b",4},  {"c",10}]},          {"c",[{"a",12}, {"c",20}]}]),
+    ?assertEqual( M7, [{"a",[{"b",4},  {"c",10}]},          {"c",[{"a",5},  {"c",50}]}]),
+    ?assertEqual( M8, [{"a",[{"a",12}, {"b",7}, {"c",10}]}, {"c",[{"c",20}]}]),
+    ?assertEqual( M9, [{"a",[{"a",5},  {"b",5}, {"c",50}]}, {"c",[{"c",20}]}]).
+
 
 min_test() ->
     M = new(),
-    M1 = add(M, "a", {"b",4}),
-    M2 = add(M1, "a", {"c",10}),
-    M3 = add(M2, "c", {"c",2}),
-    M4 = add(M3, "c", {"c",20}),
+    M1 = add(M, "a", "b",4),
+    M2 = add(M1, "a", "c",10),
+    M3 = add(M2, "c", "c",2),
+    M4 = add(M3, "c", "c",20),
     ?assertEqual( min(M, "a"), 0),
     ?assertEqual( min(M1, "a"), 4),
     ?assertEqual( min(M1, "b"), 0),
@@ -103,11 +129,11 @@ min_test() ->
 
 peers_test() ->
     M = new(),
-    M1 = add(M, "a", {"b",4}),
-    M2 = add(M1, "a", {"c",10}),
-    M3 = add(M2, "c", {"c",2}),
-    M4 = add(M3, "c", {"c",20}),
-    M5 = add(M4, "c", {"c",15}),
+    M1 = add(M, "a", "b",4),
+    M2 = add(M1, "a", "c",10),
+    M3 = add(M2, "c", "c",2),
+    M4 = add(M3, "c", "c",20),
+    M5 = add(M4, "c", "c",15),
     ?assertEqual( peers(M), []),
     ?assertEqual( peers(M1), ["a"]),
     ?assertEqual( peers(M5), ["a", "c"]).
@@ -115,10 +141,10 @@ peers_test() ->
 
 get_test() ->
     M = new(),
-    M1 = add(M, "a", {"b",4}),
-    M2 = add(M1, "a", {"c",10}),
-    M3 = add(M2, "c", {"c",2}),
-    M4 = add(M3, "c", {"c",20}),
+    M1 = add(M, "a", "b",4),
+    M2 = add(M1, "a", "c",10),
+    M3 = add(M2, "c", "c",2),
+    M4 = add(M3, "c", "c",20),
     ?assertEqual( get(M, "a", "a"), 0),
     ?assertEqual( get(M1, "a", "a"), 0),
     ?assertEqual( get(M1, "b", "a"), 0),
@@ -127,10 +153,10 @@ get_test() ->
 
 reset_counters_test() ->
     M = new(),
-    M1 = add(M, "a", {"b",4}),
-    M2 = add(M1, "a", {"c",10}),
-    M3 = add(M2, "c", {"c",2}),
-    M4 = add(M3, "c", {"c",20}),
+    M1 = add(M, "a", "b",4),
+    M2 = add(M1, "a", "c",10),
+    M3 = add(M2, "c", "c",2),
+    M4 = add(M3, "c", "c",20),
     ?assertEqual( reset_counters(M), M),
     ?assertEqual( reset_counters(M1), [{"a",[{"b",0}]}]),
     ?assertEqual( reset_counters(M2), [{"a",[{"b",0}, {"c",0}]}]),
@@ -139,10 +165,10 @@ reset_counters_test() ->
 
 delete_peer_test() ->
     M = new(),
-    M1 = add(M, "a", {"b",4}),
-    M2 = add(M1, "a", {"c",10}),
-    M3 = add(M2, "c", {"c",2}),
-    M4 = add(M3, "c", {"c",20}),
+    M1 = add(M, "a", "b",4),
+    M2 = add(M1, "a", "c",10),
+    M3 = add(M2, "c", "c",2),
+    M4 = add(M3, "c", "c",20),
     ?assertEqual( delete_peer(M1, "a"), []),
     ?assertEqual( delete_peer(M1, "b"), [{"a",[]}]),
     ?assertEqual( delete_peer(M1, "c"), [{"a",[{"b",4}]}]),
